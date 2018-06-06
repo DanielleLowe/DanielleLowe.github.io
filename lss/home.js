3e7894e9c6df6e2a1d8299893744af9b7d080481
DanielleLowe <dlowe15@horizon.csueastbay.edu>
2018-06-05 22:29:31 -0700

    changing everything

--- lss/home.js
+++ lss/home.js
@@ -1,22 +1,32 @@
+var canvas = document.getElementById("canvas");
 var sheep = document.getElementById("sheep");
+var icon = document.getElementById("icon");
 
- window.onscroll = function(){
+window.onscroll = function(){
 
-  // console.log("yo, yo, yo");
 
-  var fromTop = window.pageYOffset;
+  var y = window.pageYOffset;
 
-  // console.log(fromTop);
+  console.log(y);
 
-  sheep.innerHTML = fromTop + "px";
+  sheep.style.top = 400 + y * .97 + "px";
 
-  sheep.style.top = fromTop *1.02 + "px";
+  sheep.innerHTML = "sheep";
 
-  // submarine.style.transform="rotate("+ fromTop + "deg)";
-   };
+  if (y > 2000) {
+    sheep.style.backgroundColor = "green";
+  }else{
+    sheep.style.backgroundColor = "blue";}
 
-   $(document).ready(function(){
-    $("bird").onscroll(function(){
-    $("bird").animate({left: '250px'});
-    });
-});
+  icon.style.top = 200 + y  + "px";
+
+  if(y > 2000){
+
+      // icon.src="https://2.bp.blogspot.com/-MTmoGMeE2cc/WSGIutFSHoI/AAAAAAAAK6o/Ya-tX6NvAqUM2LqV4VlxzsfwzD8xXEPFACLcB/s1600/254___thumbs-up-emoji.png";
+
+  }else{
+
+  // icon.src="https://1.bp.blogspot.com/-jnt2C7f2HH8/WSGIun5uhGI/AAAAAAAAK6s/A1R4fUrtMUsQtEbKqopa2xTWqaEtxClogCLcB/s1600/256___come-to-fight-emoji.png";
+ }
+
+};
